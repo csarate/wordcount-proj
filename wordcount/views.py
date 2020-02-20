7fafbd78b01c21a8a8ea553cfdc3b4549d0fdacd
csarate <csarate@gmail.com>
2020-02-20 12:38:41 -0300

    Primer commit de WordCount

---
+++ wordcount/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+import operator
+
+
+def homepage(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def count(request):
+    fulltext = request.GET['fulltext']
+
+    wordlist = fulltext.split()
+
+    worddictionary = {}
+
+    for word in wordlist:
+        if word in worddictionary:
+            # Incrementa
+            worddictionary[word] += 1
+        else:
+            # suma al diccionario
+            worddictionary[word] = 1
+
+    sortedwords = sorted(
+        worddictionary.items(), key=operator.itemgetter(1),
+        reverse=True
+        )
+
+    return render(
+        request, 'count.html',
+        {'fulltext': fulltext, 'count': len(wordlist),
+         'sortedwords': sortedwords}
+         )
